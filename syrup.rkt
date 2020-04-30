2881777d41cab5ad04e48fc9bc3b8b2c173b4634
Christopher Lemmer Webber <cwebber@dustycloud.org>
2020-04-30 12:14:38 -0400

    Syrup!  Now with strings

---
+++ syrup.rkt
@@ -0,0 +1,155 @@
+#lang racket/base
+
+(require racket/match
+         racket/set)
+
+(define (netstring-encode bstr)
+  (bytes-append (string->bytes/latin-1 (number->string (bytes-length bstr)))
+                #":"
+                bstr))
+
+(define (syrup-encode obj)
+  
+  (match obj
+    ;; Bytes are like <bytes-len>:<bytes>
+    [(? bytes?)
+     (netstring-encode obj)]
+    ;; Integers are like i<maybe-signed-integer>e
+    [(? integer?)
+     (bytes-append #"i" (string->bytes/latin-1 (number->string obj)) #"e")]
+    ;; Lists are like l<item1><item2><item3>e
+    [(? list?)
+     (bytes-append #"l"
+                   (apply bytes-append
+                          (map syrup-encode obj))
+                   #"e")]
+    ;; Dictionaries are like d<key1><val1><key2><val2>e
+    [(? hash?)
+     (define sorted-keys
+       (sort (hash-keys obj) bytes<?))
+     (define encoded-hash-pairs
+       (for/list ([key sorted-keys])
+         (define val
+           (hash-ref obj key))
+         (bytes-append (syrup-encode key)
+                       (syrup-encode val))))
+     (bytes-append #"d"
+                   (apply bytes-append encoded-hash-pairs)
+                   #"e")]
+    [(? string?)
+     (bytes-append #"\""
+                   (netstring-encode (string->bytes/utf-8 obj)))]
+    ;; wtf is this
+    [_ (error 'syrup-unsupported-type obj)]))
+
+(define digit-chars
+  (seteq #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
+
+(define (digit-char? char)
+  (set-member? digit-chars char))
+
+(define (read-netstring in-port)
+  (define bytes-len
+    (string->number
+     (list->string
+      (let lp ()
+        (match (read-char in-port)
+          [#\:
+           '()]
+          [(? digit-char? digit-char)
+           (cons digit-char
+                 (lp))])))))
+  (read-bytes bytes-len in-port))
+
+(define (syrup-read in-port)
+  (match (peek-char in-port)
+    ;; it's a bytestring
+    [(? digit-char?)
+     (read-netstring in-port)]
+    ;; it's an integer
+    [#\i
+     (read-byte in-port)
+     (define negative?
+       (if (eq? (peek-char in-port) #\-)
+           (begin
+             (read-byte in-port)
+             #t)
+           #f))
+     (define num
+       (string->number
+        (list->string
+         (let lp ()
+           ;; TODO: more error handling here
+           (match (read-char in-port)
+             [#\e
+              '()]
+             [(? digit-char? digit-char)
+              (cons digit-char
+                    (lp))])))))
+     (if negative?
+         (* num -1)
+         num)]
+    ;; it's a list
+    [#\l
+     (read-byte in-port)
+     (let lp ()
+       (match (peek-char in-port)
+         ;; We've reached the end
+         [#\e
+          (read-byte in-port)
+          '()]
+         ;; one more loop
+         [_
+          (cons (syrup-read in-port) (lp))]))]
+    [#\d
+     (read-byte in-port)
+     (let lp ([ht #hash()])
+       (match (peek-char in-port)
+         [#\e
+          (read-byte in-port)
+          ht]
+         [_
+          (define key
+            (syrup-read in-port))
+          (unless (bytes? key)
+            (error 'syrup-key-not-string key))
+          (define val
+            (syrup-read in-port))
+          (lp (hash-set ht key val))]))]
+    [#\"
+     (read-byte in-port)
+     (bytes->string/utf-8 (read-netstring in-port))]
+    [_
+     (error 'syrup-invalid-char "Unexpected character at position ~a: ~a"
+            (file-position in-port)
+            (peek-char in-port))]))
+
+(define (syrup-decode bstr)
+  (syrup-read (open-input-bytes bstr)))
+
+
+(module+ test
+  (require rackunit)
+
+  (define zoo-structure
+    '(#"zoo"
+      #hash((#"species" . #"cat")
+            (#"name" . "Tabatha")
+            (#"age" . 12)
+            (#"eats" . (#"mice" #"fish" #"kibble")))
+      #hash((#"species" . #"monkey")
+            (#"name" . "George")
+            (#"age" . 6)
+            (#"eats" . (#"bananas" #"insects")))))
+
+  (define zoo-expected-bytes
+    #"l3:zood3:agei12e4:eatsl4:mice4:fish6:kibblee4:name\"7:Tabatha7:species3:cated3:agei6e4:eatsl7:bananas7:insectse4:name\"6:George7:species6:monkeyee")
+  (test-equal?
+   "Correctly encodes zoo structure"
+   (syrup-encode zoo-structure)
+   zoo-expected-bytes)
+  
+  (test-equal?
+   "Correctly decodes zoo structure"
+   (syrup-decode zoo-expected-bytes)
+   zoo-structure))
