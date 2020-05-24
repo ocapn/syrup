02a54853fd62e7fa894e8c9ffe0852e2dd27c04c
Christopher Lemmer Webber <cwebber@dustycloud.org>
2020-05-24 18:10:52 -0400

    add recordifier and derecordifier for encoding and decoding

--- syrup.rkt
+++ syrup.rkt
@@ -5,7 +5,8 @@
          syrup-read)
 
 (require racket/match
-         racket/set)
+         racket/set
+         racket/contract)
 
 (struct record (label args)
   #:transparent)
@@ -30,82 +31,97 @@
 ;; Records: <<label><val1><val2><val3>> (the outer <> for realsies tho)
 ;; Sets: #<item1><item2><item3>$
 
-(define (syrup-encode obj)
-  (match obj
-    ;; Bytes are like <bytes-len>:<bytes>
-    [(? bytes?)
-     (netstring-encode obj)]
-    ;; Integers are like i<maybe-signed-integer>e
-    [(? integer?)
-     (bytes-append #"i" (string->bytes/latin-1 (number->string obj)) #"e")]
-    ;; Lists are like [<item1><item2><item3>]
-    [(? list?)
-     (bytes-append #"["
-                   (apply bytes-append
-                          (map syrup-encode obj))
-                   #"]")]
-    ;; Dictionaries are like {<key1><val1><key2><val2>}
-    ;; We sort by the key being fully encoded.
-    [(? hash?)
-     (define keys-and-encoded
-       (for/list ([key (hash-keys obj)])
-         (cons (syrup-encode key) key)))
-     (define sorted-keys-and-encoded
-       (sort keys-and-encoded
-             (match-lambda*
-               [(list (and ke1 (cons encoded1 _k1))
-                      (and ke2 (cons encoded2 _k2)))
-                (bytes<? encoded1 encoded2)])))
-     (define encoded-hash-pairs
-       (for/list ([ke sorted-keys-and-encoded])
-         (match ke
-           [(cons enc-key key)
-            (define val
-              (hash-ref obj key))
-            (bytes-append enc-key (syrup-encode val))])))
-     (bytes-append #"{"
-                   (apply bytes-append encoded-hash-pairs)
-                   #"}")]
-    ;; Strings are like <encoded-bytes-len>"<utf8-encoded>
-    [(? string?)
-     (netstring-encode (string->bytes/utf-8 obj)
-                       #:joiner #"\"")]
-    ;; Symbols are like <encoded-bytes-len>'<utf8-encoded>
-    [(? symbol?)
-     (netstring-encode (string->bytes/utf-8
-                        (symbol->string obj))
-                       #:joiner #"'")]
-    ;; TODO: We don't allow infinity, NaN, etc here, right...?
-    ;; Single flonum floats are like F<big-endian-encoded-single-float>
-    [(and (? single-flonum?) (? real?))
-     (bytes-append #"F"
-                   (real->floating-point-bytes obj 4 #t))]
-    ;; Double flonum floats are like D<big-endian-encoded-double-float>
-    [(and (? double-flonum?) (? real?))
-     (bytes-append #"D"
-                   (real->floating-point-bytes obj 8 #t))]
-    ;; Records are like <<tag><arg1><arg2>> but with the outer <> for realsies
-    [(? record?)
-     (bytes-append #"<"
-                   (syrup-encode (record-label obj))
-                   (apply bytes-append
-                          (map syrup-encode (record-args obj)))
-                   #">")]
-    ;; #t is t, #f is f
-    [#t #"t"]
-    [#f #"f"]
-    ;; Sets are like #<item1><item2><item3>$
-    [(? set?)
-     (define encoded-items
-       (for/list ([item obj])
-         (syrup-encode item)))
-     (define sorted-items
-       (sort encoded-items
-             bytes<?))
-     (bytes-append #"#"
-                   (apply bytes-append sorted-items)
-                   #"$")]
-    [_ (error 'syrup-unsupported-type obj)]))
+(define (syrup-encode obj
+                      ;; an alist of (predicate . recordifier)... translates unknown
+                      ;; object into a representation we can understand
+                      #:recordifiers [recordifiers '()])
+  (define (encode obj)
+    (match obj
+      ;; Bytes are like <bytes-len>:<bytes>
+      [(? bytes?)
+       (netstring-encode obj)]
+      ;; Integers are like i<maybe-signed-integer>e
+      [(? integer?)
+       (bytes-append #"i" (string->bytes/latin-1 (number->string obj)) #"e")]
+      ;; Lists are like [<item1><item2><item3>]
+      [(? list?)
+       (bytes-append #"["
+                     (apply bytes-append
+                            (map encode obj))
+                     #"]")]
+      ;; Dictionaries are like {<key1><val1><key2><val2>}
+      ;; We sort by the key being fully encoded.
+      [(? hash?)
+       (define keys-and-encoded
+         (for/list ([key (hash-keys obj)])
+           (cons (encode key) key)))
+       (define sorted-keys-and-encoded
+         (sort keys-and-encoded
+               (match-lambda*
+                 [(list (and ke1 (cons encoded1 _k1))
+                        (and ke2 (cons encoded2 _k2)))
+                  (bytes<? encoded1 encoded2)])))
+       (define encoded-hash-pairs
+         (for/list ([ke sorted-keys-and-encoded])
+           (match ke
+             [(cons enc-key key)
+              (define val
+                (hash-ref obj key))
+              (bytes-append enc-key (encode val))])))
+       (bytes-append #"{"
+                     (apply bytes-append encoded-hash-pairs)
+                     #"}")]
+      ;; Strings are like <encoded-bytes-len>"<utf8-encoded>
+      [(? string?)
+       (netstring-encode (string->bytes/utf-8 obj)
+                         #:joiner #"\"")]
+      ;; Symbols are like <encoded-bytes-len>'<utf8-encoded>
+      [(? symbol?)
+       (netstring-encode (string->bytes/utf-8
+                          (symbol->string obj))
+                         #:joiner #"'")]
+      ;; Single flonum floats are like F<big-endian-encoded-single-float>
+      [(? single-flonum?)
+       (bytes-append #"F"
+                     (real->floating-point-bytes obj 4 #t))]
+      ;; Double flonum floats are like D<big-endian-encoded-double-float>
+      [(? double-flonum?)
+       (bytes-append #"D"
+                     (real->floating-point-bytes obj 8 #t))]
+      ;; Records are like <<tag><arg1><arg2>> but with the outer <> for realsies
+      [(? record?)
+       (bytes-append #"<"
+                     (encode (record-label obj))
+                     (apply bytes-append
+                            (map encode (record-args obj)))
+                     #">")]
+      ;; #t is t, #f is f
+      [#t #"t"]
+      [#f #"f"]
+      ;; Sets are like #<item1><item2><item3>$
+      [(? set?)
+       (define encoded-items
+         (for/list ([item obj])
+           (encode item)))
+       (define sorted-items
+         (sort encoded-items
+               bytes<?))
+       (bytes-append #"#"
+                     (apply bytes-append sorted-items)
+                     #"$")]
+      [_
+       (call/ec
+        (lambda (return)
+          (for ([recordifier recordifiers])
+            (match recordifier
+              [(cons handles-it? translate)
+               (when (handles-it? obj)
+                 (define translated (translate obj))
+                 (if (record? translated)
+                     (return (encode translated))
+                     (error 'syrup-recordifier-returned-unsupported-type)))]))
+          (error 'syrup-unsupported-type obj)))]))
+  (encode obj))
 
 (define digit-chars
   (seteq #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
@@ -116,153 +132,173 @@
 (define (digit-char? char)
   (set-member? digit-chars char))
 
-(define (syrup-read in-port)
-  ;; consume whitespace
-  (let lp ()
-    (when (set-member? whitespace-chars (peek-char in-port))
-      (read-char in-port)
-      (lp)))
+(define (syrup-read in-port
+                    ;; inverse of syrup-encode's recordifiers;
+                    ;; alist of (label-pred? . derecorify)
+                    #:derecordifiers [derecordifiers '()])
+  (define (read-next)
+    ;; consume whitespace
+    (let lp ()
+      (when (set-member? whitespace-chars (peek-char in-port))
+        (read-char in-port)
+        (lp)))
 
-  (match (peek-char in-port)
-    ;; it's either a bytestring, a symbol, or a string...
-    ;; we tell via the divider
-    [(? digit-char?)
-     (define type #f)
-     (define bytes-len
-       (string->number
-        (list->string
-         (let lp ()
-           (match (read-char in-port)
-             [#\:
-              (set! type 'bstr)
-              '()]
-             [#\'
-              (set! type 'sym)
-              '()]
-             [#\"
-              (set! type 'str)
-              '()]
-             [(? digit-char? digit-char)
-              (cons digit-char
-                    (lp))]
-             [other-char
-              (error 'syrup-invalid-digit
-                     "Invalid digit at pos ~a: ~a"
-                     (file-position in-port)
-                     other-char)])))))
-     (define bstr
-       (read-bytes bytes-len in-port))
-     (match type
-       ['bstr
-        bstr]
-       ['sym
-        (string->symbol (bytes->string/utf-8 bstr))]
-       ['str
-        (bytes->string/utf-8 bstr)])]
-    ;; it's an integer
-    [#\i
-     (read-byte in-port)
-     (define negative?
-       (if (eq? (peek-char in-port) #\-)
-           (begin
-             (read-byte in-port)
-             #t)
-           #f))
-     (define num
-       (string->number
-        (list->string
-         (let lp ()
-           ;; TODO: more error handling here
-           (match (read-char in-port)
-             [#\e
-              '()]
-             [(? digit-char? digit-char)
-              (cons digit-char
-                    (lp))]
-             [other-char
-              (error 'syrup-invalid-digit
-                     "Invalid digit at pos ~a: ~a"
-                     (sub1 (file-position in-port))
-                     other-char)])))))
-     (if negative?
-         (* num -1)
-         num)]
-    ;; it's a list
-    [(or #\[ #\( #\l)
-     (read-byte in-port)
-     (let lp ()
-       (match (peek-char in-port)
-         ;; We've reached the end
-         [(or #\] #\) #\e)
-          (read-byte in-port)
-          '()]
-         ;; one more loop
-         [_
-          (cons (syrup-read in-port) (lp))]))]
-    ;; it's a hashmap/dictionary
-    [(or #\{ #\d)
-     (read-byte in-port)
-     (let lp ([ht #hash()])
-       (match (peek-char in-port)
-         [(or #\} #\e)
-          (read-byte in-port)
-          ht]
-         [_
-          (define key
-            (syrup-read in-port))
-          (define val
-            (syrup-read in-port))
-          (lp (hash-set ht key val))]))]
-    ;; it's a record
-    [#\<
-     (read-byte in-port)
-     (define label
-       (syrup-read in-port))
-     (define args
+    (match (peek-char in-port)
+      ;; it's either a bytestring, a symbol, or a string...
+      ;; we tell via the divider
+      [(? digit-char?)
+       (define type #f)
+       (define bytes-len
+         (string->number
+          (list->string
+           (let lp ()
+             (match (read-char in-port)
+               [#\:
+                (set! type 'bstr)
+                '()]
+               [#\'
+                (set! type 'sym)
+                '()]
+               [#\"
+                (set! type 'str)
+                '()]
+               [(? digit-char? digit-char)
+                (cons digit-char
+                      (lp))]
+               [other-char
+                (error 'syrup-invalid-digit
+                       "Invalid digit at pos ~a: ~a"
+                       (file-position in-port)
+                       other-char)])))))
+       (define bstr
+         (read-bytes bytes-len in-port))
+       (match type
+         ['bstr
+          bstr]
+         ['sym
+          (string->symbol (bytes->string/utf-8 bstr))]
+         ['str
+          (bytes->string/utf-8 bstr)])]
+      ;; it's an integer
+      [#\i
+       (read-byte in-port)
+       (define negative?
+         (if (eq? (peek-char in-port) #\-)
+             (begin
+               (read-byte in-port)
+               #t)
+             #f))
+       (define num
+         (string->number
+          (list->string
+           (let lp ()
+             ;; TODO: more error handling here
+             (match (read-char in-port)
+               [#\e
+                '()]
+               [(? digit-char? digit-char)
+                (cons digit-char
+                      (lp))]
+               [other-char
+                (error 'syrup-invalid-digit
+                       "Invalid digit at pos ~a: ~a"
+                       (sub1 (file-position in-port))
+                       other-char)])))))
+       (if negative?
+           (* num -1)
+           num)]
+      ;; it's a list
+      [(or #\[ #\( #\l)
+       (read-byte in-port)
        (let lp ()
          (match (peek-char in-port)
-           [#\> '()]
-           [_ (cons (syrup-read in-port) (lp))])))
-     (record label args)]
-    ;; it's a single float
-    [#\F
-     (read-byte in-port)
-     (let ([val (floating-point-bytes->real (read-bytes 4 in-port) #t)])
-       (unless (real? val)
-         (error 'not-a-real-number val))
-       val)]
-    ;; it's a double float
-    [#\D
-     (read-byte in-port)
-     (let ([val (floating-point-bytes->real (read-bytes 8 in-port) #t)])
-       (unless (real? val)
-         (error 'not-a-real-number val))
-       val)]
-    ;; it's a boolean
-    [#\t
-     (read-byte in-port)
-     #t]
-    [#\f
-     (read-byte in-port)
-     #f]
-    ;; it's a set
-    [#\#
-     (read-byte in-port)
-     (let lp ([s (set)])
-       (match (peek-char in-port)
-         [#\$
-          (read-byte in-port)
-          s]
-         [_
-          (lp (set-add s (syrup-read in-port)))]))]
-    [_
-     (error 'syrup-invalid-char "Unexpected character at position ~a: ~a"
-            (file-position in-port)
-            (peek-char in-port))]))
+           ;; We've reached the end
+           [(or #\] #\) #\e)
+            (read-byte in-port)
+            '()]
+           ;; one more loop
+           [_
+            (cons (read-next) (lp))]))]
+      ;; it's a hashmap/dictionary
+      [(or #\{ #\d)
+       (read-byte in-port)
+       (let lp ([ht #hash()])
+         (match (peek-char in-port)
+           [(or #\} #\e)
+            (read-byte in-port)
+            ht]
+           [_
+            (define key
+              (read-next))
+            (define val
+              (read-next))
+            (lp (hash-set ht key val))]))]
+      ;; it's a record
+      [#\<
+       (read-byte in-port)
+       (define label
+         (read-next))
+       (define args
+         (let lp ()
+           (match (peek-char in-port)
+             [#\>
+              (read-byte in-port)
+              '()]
+             [_ (cons (read-next) (lp))])))
+       (call/ec
+        (lambda (return)
+          (for ([derecordifier derecordifiers])
+            (match derecordifier
+              [(cons (and (? (or/c symbol? string? number? boolean? bytes?))
+                          expected-label)
+                     derecordify)
+               (when (equal? label expected-label)
+                 (return (apply derecordify args)))]
+              [(cons label-pred? derecordify)
+               (when (label-pred? label)
+                 (return (apply derecordify args)))]))
+          ;; no handler, return as record
+          (record label args)))]
+      ;; it's a single float
+      [#\F
+       (read-byte in-port)
+       (let ([val (floating-point-bytes->real (read-bytes 4 in-port) #t)])
+         (unless (real? val)
+           (error 'not-a-real-number val))
+         val)]
+      ;; it's a double float
+      [#\D
+       (read-byte in-port)
+       (let ([val (floating-point-bytes->real (read-bytes 8 in-port) #t)])
+         (unless (real? val)
+           (error 'not-a-real-number val))
+         val)]
+      ;; it's a boolean
+      [#\t
+       (read-byte in-port)
+       #t]
+      [#\f
+       (read-byte in-port)
+       #f]
+      ;; it's a set
+      [#\#
+       (read-byte in-port)
+       (let lp ([s (set)])
+         (match (peek-char in-port)
+           [#\$
+            (read-byte in-port)
+            s]
+           [_
+            (lp (set-add s (read-next)))]))]
+      [_
+       (error 'syrup-invalid-char "Unexpected character at position ~a: ~a"
+              (file-position in-port)
+              (peek-char in-port))]))
+  (read-next))
 
-(define (syrup-decode bstr)
-  (syrup-read (open-input-bytes bstr)))
-
+(define (syrup-decode bstr #:derecordifiers [derecordifiers '()])
+  (syrup-read (open-input-bytes bstr)
+              #:derecordifiers derecordifiers))
 
 (module+ test
   (require rackunit
@@ -329,4 +365,21 @@
   (test-equal?
    "bencode backwards compat"
    (syrup-decode #"l3:zood4:name3:cat7:species7:tabathaee")
-   '(#"zoo" #hash((#"name" . #"cat") (#"species" . #"tabatha")))))
+   '(#"zoo" #hash((#"name" . #"cat") (#"species" . #"tabatha"))))
+
+  (struct fizzbuzz (blorp blap)
+    #:transparent)
+  (define (fizzbuzz->record fb)
+    (record* 'fizzbuzz (fizzbuzz-blorp fb) (fizzbuzz-blap fb)))
+  
+  (test-equal?
+   "recordifier works"
+   (syrup-encode (list 'meep 'moop (fizzbuzz 'fizzy 'water) 'bop)
+                 #:recordifiers (list (cons fizzbuzz? fizzbuzz->record)))
+   #"[4'meep4'moop<8'fizzbuzz5'fizzy5'water>3'bop]")
+
+  (test-equal?
+   "derecordifier works"
+   (syrup-decode #"[4'meep4'moop<8'fizzbuzz5'fizzy5'water>3'bop]"
+                 #:derecordifiers (list (cons 'fizzbuzz fizzbuzz)))
+   (list 'meep 'moop (fizzbuzz 'fizzy 'water) 'bop)))
